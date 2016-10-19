1c1d21937672bf0632c614ec300c89cfa810e98f
jamespakorn <jameshosxp@hotmail.com>
2016-10-19 14:33:18 +0700

    add menu add array

---
+++ views/first1/index.php
@@ -0,0 +1,40 @@
+<?php
+/* @var $this yii\web\View */
+?>
+<h1><?php echo $title; ?></h1>
+
+<?php
+print_r($person);
+echo '<hr>';
+foreach ($person as $key => $item) {
+    echo $key . ' : ' . $item['fname'] . ' / ' . $item['lname'] . ' / ' . $item['email'] . '<br>';
+}
+echo '<hr>';
+?>
+
+<table class="table table-bordered table-hover table-striped">
+    <thead>
+        <tr class="success">
+            <th>ลำดับ</th>
+            <th>ชื่อ</th>
+            <th>นามสกุล</th>
+            <th>อีเมล์</th>
+        </tr>
+    </thead>
+    <tbody>
+
+<?php
+foreach ($person as $key => $value) {
+
+    echo '<tr>';
+    echo '<td>'.($key+1).'</td>';
+    echo '<td>'.$value['fname'].'</td>';
+    echo '<td>'.$value['lname'].'</td>';
+    echo '<td>'.$value['email'].'</td>';
+    echo '</tr>';
+}
+?>
+
+
+    </tbody>
+</table>
